956c9c24c5f7423bbc02f524ac54ed01cdcfc4b3
Nhantran2004 <115575871+Nhantran2004@users.noreply.github.com>
2025-03-11 15:37:42 +0700

    Add files via upload

--- lab_02/api.py
+++ lab_02/api.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from cipher.caesar import CaesarCipher
+from cipher.vigenere import VigenereCipher
 
 app = Flask(__name__)
 
 caesar_cipher = CaesarCipher()
+vigenere_cipher = VigenereCipher()
 
+# Các route cho Caesar Cipher
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
     data = request.json
@@ -20,6 +23,23 @@ def caesar_decrypt():
     key = int(data['key'])
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
+
+# Các route cho Vigenère Cipher
+@app.route('/api/vigenere/encrypt', methods=['POST'])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/vigenere/decrypt', methods=['POST'])
+def vigenere_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
